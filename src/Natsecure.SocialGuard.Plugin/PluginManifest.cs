9b2cab07c6831edfec57785743afc09844f406a9
Sakura Isayeki <sakura.isayeki@nodsoft.net>
2021-02-09 12:16:21 +0100

    Init Plugin Project

---
+++ src/Natsecure.SocialGuard.Plugin/PluginManifest.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+
+namespace Natsecure.SocialGuard.Plugin
+{
+	public class PluginManifest : Nodsoft.YumeChan.PluginBase.Plugin
+	{
+		public override string PluginDisplayName => "Natsecure SocialGuard (YC)";
+		public override bool PluginStealth => false;
+
+		public override async Task LoadPlugin() 
+		{
+			await base.LoadPlugin();
+		}
+
+		public override async Task UnloadPlugin()
+		{
+			await base.UnloadPlugin();
+		}
+	}
+}
